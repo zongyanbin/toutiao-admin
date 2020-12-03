8cf27c1351cf92ac1d8abb3cd64eba3fe3b9ef71
ps照 <315635152@qq.com>
2020-12-03 13:11:38 +0800

    'toutiao-admin'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
 
-Vue.config.productionTip = false
+/**
+ * 项目的启动入口
+ */
 
+import Vue from 'vue'// 加载vue
+import App from './App.vue' // 加载根组件
+import router from './router'// 加载路由
+
+Vue.config.productionTip = false // 关闭生产环境提示
+
+// 创建 vue 根实例
+// 把 router 配置到根实例中
+// 通过 render 方法把 App 根组件渲染到 #app 入口节点
 new Vue({
   router,
   render: h => h(App)
+  // el: '#app'  等价于 $mount('#app')
 }).$mount('#app')
