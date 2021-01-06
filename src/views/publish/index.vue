10e2e78f2b131263e5488653b7ab980e29c36ec2
zongyanbin <315635152@qq.com>
2021-01-07 04:49:09 +0800

    5-12请求提交

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -9,30 +9,34 @@
         </el-breadcrumb>
         <!-- /面包屑路径导航 -->
       </div>
-      <el-form ref="form" :model="form" label-width="40px">
+      <el-form ref="form" :model="article" label-width="40px">
         <el-form-item label="标题">
-          <el-input v-model="form.name"></el-input>
+          <el-input v-model="article.title"></el-input>
         </el-form-item>
         <el-form-item label="内容">
-          <el-input type="textarea" v-model="form.desc"></el-input>
+          <el-input type="textarea" v-model="article.content"></el-input>
         </el-form-item>
         <el-form-item label="封面">
-          <el-radio-group v-model="form.resource">
-            <el-radio label="单图"></el-radio>
-            <el-radio label="三图"></el-radio>
-            <el-radio label="无图"></el-radio>
-            <el-radio label="自动"></el-radio>
+          <el-radio-group v-model="article.cover.type">
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道">
-          <el-select v-model="form.region" placeholder="请选择活动区域">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
+          <el-select v-model="article.channel_id" placeholder="请选择活动区域">
+            <el-option
+              :label="channel.name"
+              :value="channel.id"
+              v-for="(channel, index) in channels"
+              :key="index"
+            ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">发表</el-button>
-          <el-button>存入草稿</el-button>
+          <el-button type="primary" @click="onPublish">发表</el-button>
+          <el-button @click="onPublish(true)">存入草稿</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -40,32 +44,50 @@
 </template>
 
 <script>
+import { getArticleChannels, addArticle } from '@/api/article'
 export default {
   name: 'PublishIndex',
   components: {},
   props: {},
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+      channels: [], // 文章频道列表
+      article: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: { // 文章封面
+          type: 0, // 封面类型 -1:自动, 0-无图, 1-1张, 3-3张
+          images: [] // 封面图片的地址
+        },
+        channel_id: null
       }
     }
   },
   watch: {},
   computed: {},
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    loadChannels () {
+      getArticleChannels().then(res => {
+        this.channels = res.data.data.channels
+      })
+    },
+    onPublish (draft = false) {
+      // 找到数据接口
+      // 封装请求方法
+      // 请求提交表单
+      addArticle(this.article, draft).then(res => {
+      // 处理响应结果
+        this.$message({
+          message: '发布成功',
+          type: 'success'
+        })
+      // console.log(res)
+      })
     }
   },
-  created () {},
+  created () {
+    this.loadChannels()
+  },
   mounted () {}
 }
 </script>
